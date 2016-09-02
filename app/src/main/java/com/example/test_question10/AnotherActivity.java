c939ab2acc7138fe011e9231389709acdc32fb6c
ghy <1724286863@qq.com>
2016-09-02 18:40:36 +0800

    [上机实践]Activity间单向传递数据 掌握在两个Activity之间如何传递数据，其中数据有String、int等多种格式。 要求： 1.掌握使用Intent携带数据。 2.掌握获得Intent携带的数据。

---
+++ app/src/main/java/com/example/test_question10/AnotherActivity.java
@@ -0,0 +1,20 @@
+package com.example.test_question10;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Intent intent =getIntent();
+        String name = intent.getStringExtra("name");
+        Integer age =intent.getIntExtra("age",20);
+        Toast.makeText(this,name+age,Toast.LENGTH_LONG).show();
+    }
+}
